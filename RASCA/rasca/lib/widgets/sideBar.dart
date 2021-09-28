d562ed3fd02ed882f98df27be3849d35bb15e61e
Karen Izabel <kpaiz@bluedevs.com>
2021-09-27 21:07:53 -0600

    Menu

---
+++ RASCA/rasca/lib/widgets/sideBar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:rasca/views/CrearActividad.dart';
+import 'package:rasca/views/UsuarioPerfil.dart';
+import 'package:rasca/views/dashboard_view.dart';
+import 'package:rasca/views/loginV2_view.dart' as login;
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Menú',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue[500],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Mis actividades'),
+            onTap: () => {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => MyApp()))
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Actividades'),
+            onTap: () => {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => MyApp()))
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Peril'),
+            onTap: () => {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => PerfilUsuario()))
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Crear actividad'),
+            onTap: () => {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Actividad()))
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => login.MyHomePage()))
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
